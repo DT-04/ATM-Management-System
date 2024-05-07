2120c53664c68ffe8b5b2531923347abe5042a99
Dhruvi Tanna <127314600+DT-04@users.noreply.github.com>
2024-05-07 15:50:03 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class ATM
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		OptionMenu optionMenu = new OptionMenu();
+		System.out.println("Welcome to the ATM Project!");
+		optionMenu.mainMenu();
+	}
+
+	
+}
